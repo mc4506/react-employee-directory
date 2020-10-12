63a711d3664c6839d4017819b44949f5624467f0
Mike S Chen <mike.sihong.chen@gmail.com>
2020-10-12 09:53:44 -0400

    Updates

---
+++ precache-manifest.de41feec67f2249ac8ceaa25588214a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b809a3c5aa73c9317ab2aba47bb1c3f",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "c7230e5a751e28843578",
+    "url": "/react-employee-directory/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "23cfffcd340cbe6221c0",
+    "url": "/react-employee-directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c7230e5a751e28843578",
+    "url": "/react-employee-directory/static/js/2.c8f7f6a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-employee-directory/static/js/2.c8f7f6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23cfffcd340cbe6221c0",
+    "url": "/react-employee-directory/static/js/main.44417bc3.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
